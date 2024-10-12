ffecbd0d33572a8d73dad696e9efba8a701f8e35
calin-cox <156495154+calin-cox@users.noreply.github.com>
2024-10-12 16:36:44 -0500

    Renamed file, added todos

--- src/main/java/io/github/calincox/mythos/MythosRegistry.java
+++ src/main/java/io/github/calincox/mythos/MythosRegistry.java
@@ -35,45 +35,58 @@ import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
 // The value here should match an entry in the META-INF/neoforge.mods.toml file
-@Mod(MythosMain.MODID)
-public class MythosMain
+@Mod(MythosRegistry.MODID)
+public class MythosRegistry
 {
     // Define mod id in a common place for everything to reference
     public static final String MODID = "mythos";
     // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
-    // Create a Deferred Register to hold Blocks which will all be registered under the "examplemod" namespace
+    // Create a Deferred Register to hold Blocks which will all be registered under the "mythos" namespace
     public static final DeferredRegister.Blocks BLOCKS = DeferredRegister.createBlocks(MODID);
-    // Create a Deferred Register to hold Items which will all be registered under the "examplemod" namespace
+    // Create a Deferred Register to hold Items which will all be registered under the "mythos" namespace
     public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(MODID);
-    // Create a Deferred Register to hold CreativeModeTabs which will all be registered under the "examplemod" namespace
+    // Create a Deferred Register to hold CreativeModeTabs which will all be registered under the "mythos" namespace
     public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, MODID);
 
-    // Creates a new Block with the id "examplemod:example_block", combining the namespace and path
-    public static final DeferredBlock<Block> marble = BLOCKS.registerSimpleBlock("marble", BlockBehaviour.Properties.of().mapColor(MapColor.STONE));
-    // Creates a new BlockItem with the id "examplemod:example_block", combining the namespace and path
-    public static final DeferredItem<BlockItem> marble_item = ITEMS.registerSimpleBlockItem("marble", marble);
+    // Creates a new Block with the id "mythos:example_block", combining the namespace and path
+   
+    //Blocks
+        
+        //Marble, the Paradigm. Notice the itemstate of the block is implemented here, rather than with items.
+    public static final DeferredBlock<Block> marble = BLOCKS.registerSimpleBlock("marble", BlockBehaviour.Properties.of().mapColor(MapColor.STONE).destroyTime(3/2).speedFactor(4/3));
+        // Creates a new BlockItem with the id "mythos:example_block", combining the namespace and path
+        public static final DeferredItem<BlockItem> marble_item = ITEMS.registerSimpleBlockItem("marble", marble);
 
-    //onyx!
-    public static final DeferredBlock<Block> onyx = BLOCKS.registerSimpleBlock("onyx", BlockBehaviour.Properties.of().isRedstoneConductor(null));
-    public static final DeferredItem<BlockItem> onyx_item = ITEMS.registerSimpleBlockItem("onyx", onyx );
+        //onyx!
+        public static final DeferredBlock<Block> onyx = BLOCKS.registerSimpleBlock("onyx", BlockBehaviour.Properties.of().isRedstoneConductor(null).jumpFactor(1/2));
+        public static final DeferredItem<BlockItem> onyx_item = ITEMS.registerSimpleBlockItem("onyx", onyx );
 
-    // Creates a new food item with the id "examplemod:example_id", nutrition 1 and saturation 2
-    public static final DeferredItem<Item> EXAMPLE_ITEM = ITEMS.registerSimpleItem("example_item", new Item.Properties().food(new FoodProperties.Builder()
+     //Items
+     
+        //Ash
+        //TODO: register this item in the creative menu.
+        public static final DeferredItem<Item> ash = ITEMS.registerSimpleItem("ash", new Item.Properties().stacksTo(24));
+        //Ichor
+        //TODO: register this item in the creative menu.
+        public static final DeferredItem<Item> ichor = ITEMS.registerSimpleItem("ichor", new Item.Properties()); 
+
+        // Creates a new food item with the id "mythos:example_id", nutrition 1 and saturation 2
+        public static final DeferredItem<Item> Ambrosia = ITEMS.registerSimpleItem("ambrosia", new Item.Properties().food(new FoodProperties.Builder()
             .alwaysEdible().nutrition(1).saturationModifier(2f).build()));
 
-    // Creates a creative tab with the id "examplemod:example_tab" for the example item, that is placed after the combat tab
+    // Creates a creative tab with the id "mythos:example_tab" for the example item, that is placed after the combat tab
     public static final DeferredHolder<CreativeModeTab, CreativeModeTab> EXAMPLE_TAB = CREATIVE_MODE_TABS.register("example_tab", () -> CreativeModeTab.builder()
-            .title(Component.translatable("itemGroup.examplemod")) //The language key for the title of your CreativeModeTab
+            .title(Component.translatable("itemGroup.mythos")) //The language key for the title of your CreativeModeTab
             .withTabsBefore(CreativeModeTabs.COMBAT)
-            .icon(() -> EXAMPLE_ITEM.get().getDefaultInstance())
+            .icon(() -> Ambrosia.get().getDefaultInstance())
             .displayItems((parameters, output) -> {
-                output.accept(EXAMPLE_ITEM.get()); // Add the example item to the tab. For your own tabs, this method is preferred over the event
+                output.accept(Ambrosia.get()); // Add the example item to the tab. For your own tabs, this method is preferred over the event
             }).build());
 
     // The constructor for the mod class is the first code that is run when your mod is loaded.
     // FML will recognize some parameter types like IEventBus or ModContainer and pass them in automatically.
-    public MythosMain(IEventBus modEventBus, ModContainer modContainer)
+    public MythosRegistry(IEventBus modEventBus, ModContainer modContainer)
     {
         // Register the commonSetup method for modloading
         modEventBus.addListener(this::commonSetup);
@@ -86,7 +99,7 @@ public class MythosMain
         CREATIVE_MODE_TABS.register(modEventBus);
 
         // Register ourselves for server and other game events we are interested in.
-        // Note that this is necessary if and only if we want *this* class (ExampleMod) to respond directly to events.
+        // Note that this is necessary if and only if we want *this* class (mythos) to respond directly to events.
         // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
         NeoForge.EVENT_BUS.register(this);
 
